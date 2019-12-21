ae247b6c55d1bcc60c945d6a3b2b3f1f32fd8e6b
Mark Brazeau <kitsapbackflow@gmail.com>
2019-12-21 20:33:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,13 @@ end
 end
 
 def find_max_value(array)
+    x = 0
+    array.length.times { |index|
+    if array[index] > x  
+    x = array[index]
+  end
+    }
     x
-    array.length.times { |count|
-    
 end
 
 def find_min_value(array)
